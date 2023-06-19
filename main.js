5b76e0d36718db2417e89b6f5daa488bdbdb8628
Ulugbek-Akmalovich-Shodimurodov <99031894+Ulugbek-Akmalovich-Shodimurodov@users.noreply.github.com>
2023-06-19 22:24:45 +0500

    finish

---
+++ main.js
@@ -0,0 +1,21 @@
+const image = document.querySelector('img')
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const link = document.querySelector('a')
+const api = `https://api.qrserver.com/v1/`
+const api2 = `create-qr-code/?size=450x450&data=`
+button.addEventListener('click', (e)=>{
+    image.src = `${api}${api2}${input.value}`
+    link.href = `${api}${api2}${input.value}`
+    console.log(image.src);
+    console.log(e);
+})
+
+const span = document.querySelector('.circle');
+
+
+window.addEventListener('change', (e)=>{
+    console.log(e);
+})
+
+
