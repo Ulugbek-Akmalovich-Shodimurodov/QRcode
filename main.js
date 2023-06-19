5d07534f0b4c32f1781dd9cebd2ede84217650f9
Ulugbek-Akmalovich-Shodimurodov <99031894+Ulugbek-Akmalovich-Shodimurodov@users.noreply.github.com>
2023-06-20 02:32:05 +0500

    finished

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const button = document.querySelector('button')
 const link = document.querySelector('a')
 const api = `https://api.qrserver.com/v1/`
 const api2 = `create-qr-code/?size=450x450&data=`
-button.addEventListener('click', (e)=>{
+input.addEventListener('input', (e)=>{
     image.src = `${api}${api2}${input.value}`
     link.href = `${api}${api2}${input.value}`
     console.log(image.src);
